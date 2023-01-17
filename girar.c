a5bce351df77d52a974db0880988a5763760d5bf
Manel2004 <71586983+Manel2004@users.noreply.github.com>
2023-01-17 17:38:54 +0100

    Add files via upload

--- girar.c
+++ girar.c
@@ -3,12 +3,20 @@
 #include <stdbool.h>
 
 
-void girar(char matriu_clau[][6])
+void girar(char matriu_clau[][6], int clau)
 {
-    int aux = matriu_clau[0][0];
+    char matriu_auxiliar[6][6];
     int i, j;
 
-   /* for (i = 0; i < 6; i++)
+    for (i = 0; i < 6; i++)
+    {
+        for (j = 0; j < 6; j++)
+        {
+            matriu_auxiliar[i][j] = matriu_clau[i][j];
+        }
+    }
+
+    for (i = 0; i < 6; i++)
     {
         for (j = 0; j < 6; j++)
         {
@@ -16,25 +24,50 @@ void girar(char matriu_clau[][6])
         }
     }
 
-    printf("\n"); */
+    printf("\n");
 
     for (i = 0; i < 6; i++)
     {
         for (j = 0; j < 6; j++)
         {
-
-            if (j+2 < 6)
-            {
-                matriu_clau[i][j] = matriu_clau[i][j+2];
+                matriu_clau[i][j] = matriu_clau[i][j+clau];
                 printf("%c", matriu_clau[i][j]);
-            }
             
  
         }
-    } 
+    }
 
-    matriu_clau[6][5] = aux;
-    printf("%c", matriu_clau[6][5]);
+
+/*   for (i = 0; i < 6; i++)
+    {
+        for (j = 0; j < 6; j++)
+        {
+            matriu_clau[i][j] = matriu_auxiliar[i][j];
+        }
+    }
+
+    printf("\n");
+
+    for (i = 0; i < 6; i++)
+    {
+        for (j = 0; j < 6; j++)
+        {
+            printf("%c", matriu_clau[i][j]);
+        }
+    }
+
+    printf("\n");   */
+
+
+
+    matriu_clau[6][2] = matriu_auxiliar[0][0];
+    matriu_clau[6][3] = matriu_auxiliar[0][1];
+    matriu_clau[6][4] = matriu_auxiliar[0][2];
+    matriu_clau[6][5] = matriu_auxiliar[0][3];
+    printf("%c", matriu_clau[6][2]);
+    printf("%c", matriu_clau[6][3]);
+    printf("%c", matriu_clau[6][4]);
+    printf("%c", matriu_clau[6][5]);          
 }
 
 
@@ -47,7 +80,7 @@ int main()
                               {'Y', 'Z', ' ', '1', '2', '3'},
                               {'4', '5', '6', '7', '8', '9'}};
     
-    girar(matriu_clau);
+    girar(matriu_clau, 4);
 
     printf("\n");
 
